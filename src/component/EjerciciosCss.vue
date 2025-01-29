975c63ada3a3487cc60628b97bcea55caf1e3bab
camilo <ecgarciap@slabcode.com>
2025-01-29 16:12:55 -0500

    manera de hacer css de forma lineal junto con la etiqueta

--- src/component/EjerciciosCss.vue
+++ src/component/EjerciciosCss.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1>hola mundo</h1>
+  <h1>titulo</h1>
+  <p style="color: red; font-size: 20px">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione repellat tempore aliquam illo
+    numquam hic soluta aliquid voluptatum consequuntur, obcaecati atque unde nihil? Repellat
+    incidunt, accusantium enim repudiandae voluptates doloribus!
+  </p>
 </template>
 
-<style scoped></style>
-o
+<style scoped>
+p {
+  color: blue;
+}
+</style>
