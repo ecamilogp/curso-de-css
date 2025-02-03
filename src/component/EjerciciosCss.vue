edbe673091c63a437f70c3799096661a66286fc5
camilo <ecgarciap@slabcode.com>
2025-02-03 10:33:03 -0500

    manejos de la propiedad border

--- src/component/EjerciciosCss.vue
+++ src/component/EjerciciosCss.vue
@@ -1,42 +1,143 @@
 <template>
-  <h1>propiedades de texto</h1>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat fugit voluptatibus,
-    repudiandae omnis vitae quaerat. Vitae ducimus hic architecto eligendi, quaerat quam ipsam quis
-    aliquam repellendus. Enim error repellat distinctio.
-  </p>
-
-  <p class="text">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda est quia repudiandae quae
-    voluptatum. Dolore rerum quos sint omnis illo unde dignissimos dolorem, error placeat minus
-    suscipit in dolor odio?
-  </p>
+  <div class="contenedor">
+    <div class="uno">uno</div>
+    <div class="dos">dos</div>
+    <div class="tres">tres</div>
+    <div class="cuatro">cuatro</div>
+    <div class="cinco">cinco</div>
+    <div class="seis">seis</div>
+    <div class="siete">siete</div>
+    <div class="ocho">ocho</div>
+    <div class="nueve">nueve</div>
+    <div class="diez">diez</div>
+    <div class="once">once</div>
+    <div class="doce">doce</div>
+  </div>
+  <form>
+    <input type="email" placeholder="email" />
+    <input type="password" placeholder="contraseña" />
+    <input type="submit" value="iniciar sesion" />
+  </form>
 </template>
 
 <style scoped>
-p {
-  color: crimson;
-  font-family: sans-serif;
-  font-size: 20px;
-  font-weight: bolder;
-  font-style: italic;
-  font-variant: normal;
-
-  /* podemos usar todo en una sola linea con font
-   pero es importante conservar el orden para poder hacer el efecto cascada
-
-  font: italic small-caps bold 20px sans-serif; */
+.contenedor {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  margin: auto;
+}
+.uno {
+  background: yellow;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: solid;
+  border-color: orange;
+}
+.dos {
+  background: blue;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: dotted;
+  border-color: orange;
+}
+.tres {
+  background: red;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: dashed;
+  border-color: orange;
+}
+.cuatro {
+  background: gray;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: double;
+  border-color: orange;
+}
+.cinco {
+  background: aqua;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: groove;
+  border-color: orange;
+}
+.seis {
+  background: rgb(198, 1, 198);
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: ridge;
+  border-color: orange;
+}
+.siete {
+  background: rgb(252, 199, 252);
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: inset;
+  border-color: orange;
+}
+.ocho {
+  background: rgb(190, 240, 190);
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  border-width: 9px;
+  border-style: outset;
+  border-color: orange;
 }
 
-.text {
-  color: blue;
+.nueve {
+  background: rgb(242, 255, 4);
+  width: 180px;
+  height: 180px;
+  border-top: 9px solid orange;
+  border-radius: 30%;
   text-align: center;
-  text-decoration: underline;
-  text-transform: capitalize;
-  letter-spacing: 2px;
-  word-spacing: 2px;
-  line-height: 1.5;
-  white-space: normal;
-  text-shadow: 1px 2px 2px black;
+}
+.diez {
+  background: rgb(3, 171, 255);
+  width: 180px;
+  height: 180px;
+  border-right: 9px dotted orange;
+  border-radius: 100%;
+  text-align: center;
+}
+.once {
+  background: rgb(255, 4, 4);
+  width: 180px;
+  height: 180px;
+  border-left: 9px dashed orange;
+  border-radius: 10%;
+  text-align: center;
+}
+.doce {
+  background: rgb(188, 194, 196);
+  width: 180px;
+  height: 180px;
+  border-bottom: 9px double orange;
+  border-radius: 100%;
+  text-align: center;
+}
+input {
+  width: 80%;
+  margin-top: 10px;
+}
+form {
+  width: 300px;
 }
 </style>
