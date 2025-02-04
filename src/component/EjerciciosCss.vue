7f967e427488c4029b258a15dbbf1d745fdc8a56
camilo <ecgarciap@slabcode.com>
2025-02-04 11:17:40 -0500

    ejercicio de tarjetas

--- src/component/EjerciciosCss.vue
+++ src/component/EjerciciosCss.vue
@@ -1,1 +1,190 @@
-<template>proyecto</template>
+<script setup lang="ts">
+import yotas from '@/assets/yotas.jpg'
+</script>
+<template>
+  <div class="card">
+    <img class="card-img" :src="yotas" alt="" />
+    <h1 class="title">Edward Garcia</h1>
+    <h2 class="subtitle">aprendiz frontend</h2>
+    <p class="content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    </p>
+  </div>
+  <div class="green_card">
+    <img class="card-img" :src="yotas" alt="" />
+    <h1 class="title">Edward Garcia</h1>
+    <h2 class="subtitle">aprendiz frontend</h2>
+    <p class="content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    </p>
+  </div>
+
+  <div class="red_card">
+    <img class="card-img" :src="yotas" alt="" />
+    <h1 class="title">Edward Garcia</h1>
+    <h2 class="subtitle">aprendiz frontend</h2>
+    <p class="content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    </p>
+  </div>
+  <div class="yellow_card">
+    <img class="card-img" :src="yotas" alt="" />
+    <h1 class="title">Edward Garcia</h1>
+    <h2 class="subtitle">aprendiz frontend</h2>
+    <p class="content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id tempore iure minima officia
+      quisquam ratione natus eligendi, non soluta excepturi autem hic consequuntur fuga, facere ipsa
+      optio possimus perspiciatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
+      fuga tenetur unde dolor dolorum minus modi, expedita quaerat nemo quibusdam ipsa vitae quas
+      dicta aut, qui optio fugit nisi non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  color: white;
+  background: linear-gradient(blue, black);
+  text-align: center;
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  padding: 20px 10px;
+  box-shadow: 0px 0px 25px black;
+  border-radius: 8px;
+  display: inline-block;
+  margin: 20px;
+}
+.green_card {
+  color: white;
+  background: linear-gradient(rgb(255, 17, 17), black);
+  text-align: center;
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  padding: 20px 10px;
+  box-shadow: 0px 0px 25px black;
+  border-radius: 8px;
+  display: inline-block;
+  margin: 20px;
+}
+.red_card {
+  color: white;
+  background: linear-gradient(rgb(46, 199, 43), black);
+  text-align: center;
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  padding: 20px 10px;
+  box-shadow: 0px 0px 25px black;
+  border-radius: 8px;
+  display: inline-block;
+  margin: 20px;
+}
+.yellow_card {
+  color: white;
+  background: linear-gradient(rgb(230, 237, 11), black);
+  text-align: center;
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  padding: 20px 10px;
+  box-shadow: 0px 0px 25px black;
+  border-radius: 8px;
+  display: inline-block;
+  margin: 20px;
+}
+.title {
+  font-size: 22px;
+  margin: 0;
+}
+.subtitle {
+  font-size: 13px;
+  font-weight: 100;
+  letter-spacing: 2px;
+  margin-top: 5px;
+}
+
+.content {
+  font-size: 10px;
+  padding: 10px;
+  text-align: justify;
+  margin-bottom: 0;
+}
+.card-img {
+  width: 150px;
+  border-radius: 50%;
+}
+</style>
